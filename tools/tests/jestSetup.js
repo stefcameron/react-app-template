9261a136156aac6abf711b37ed89908fe524496f
Stefan Cameron <stefan@stefcameron.com>
2023-08-13 18:36:10 -0500

    Update all dependencies to latest versions

--- tools/tests/jestSetup.js
+++ tools/tests/jestSetup.js
@@ -1,6 +1,6 @@
 // add custom DOM expectations that work well with the Testing Library
 //  (just importing is enough)
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 // add custom `toHaveNoViolations` matcher (just importing is enough)
 import 'jest-axe/extend-expect';
